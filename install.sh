43a7de2f6a47700aae4dfbb419917926c860fa78
Assis Ngolo <figassis@gmail.com>
2019-11-26 09:20:32 +0100

    fixed install

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 VERSION=$(git describe --tags)
 PACKAGE=github.com/darxkies/k8s-tew
 
-sudo mkdir -p /usr/local/bin
 mkdir -p embedded
 packr
 
-CGO_ENABLED=0 go build -ldflags "-X ${PACKAGE}/version.Version=${VERSION} -s -w" -o k8s-tew ${PACKAGE}/cmd/k8s-tew
-sudo mv k8s-tew /usr/local/bin
+CGO_ENABLED=0 go install -i -ldflags "-X ${PACKAGE}/version.Version=${VERSION} -s -w" ${PACKAGE}/cmd/k8s-tew
